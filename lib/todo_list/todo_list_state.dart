2b7a14b5931ebfeb054f3192d542919227dd0255
Wansuko-cmd <63241922+Wansuko-cmd@users.noreply.github.com>
2021-03-10 00:41:54 +0900

    [add] listを追加

--- lib/todo_list/todo_list_state.dart
+++ lib/todo_list/todo_list_state.dart
@@ -2,17 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:todo_app/todo_list/todo_list.dart';
 
 class TodoListState extends State<TodoList>{
+  List _list = [1, 2, 3, 4, 5, 6];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(
-            fontSize: 50
-          ),
-        ),
-      )
+      body: _showList()
+    );
+  }
+
+  Widget _showList() {
+    return ListView.separated(
+      itemBuilder: (BuildContext context, int index) {
+      return _contents(index);
+    },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(
+          thickness: 3.0,
+        );
+      },
+    itemCount: _list.length,);
+  }
+
+  Widget _contents(index) {
+    return ListTile(
+      title: Text(_list[index].toString()),
     );
   }
 }
